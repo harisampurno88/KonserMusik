9219189e2c1384a17e78e94a4470efba50ef611f
HariSampurno <harisampurno2@gmail.com>
2025-06-29 18:05:56 +0700

    menambahkan table tiket dan lokasi, masing" ditambahkan migrate dan model

---
+++ app/Models/lokasi.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class lokasi extends Model
+{
+    protected $table = 'lokasi';
+    protected $primaryKey = 'id_lokasi';
+    public $timestamps = false;
+
+    protected $fillable = [
+        'id_lokasi',
+        'nama_lokasi',
+        'alamat',
+    ];
+}
---
+++ app/Models/tiket.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class tiket extends Model
+{
+    protected $table = 'tiket';
+    protected $primaryKey = 'id_tiket';
+    public $timestamps = false;
+
+    protected $fillable = [
+        'id_tiket',
+        'id_konser',
+        'harga',
+        'stok',
+    ];
+}
